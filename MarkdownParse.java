30f7602458131221c9c4508d873c12489cc7c422
MichinoriW <mwada@ucsd.edu>
2022-05-16 17:13:38 -0700

    Update MarkdownParse.java
    im not sure... I just deleted a comment it seems

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -8,7 +8,6 @@ import java.util.ArrayList;
 public class MarkdownParse {
 
     public static ArrayList<String> getLinks(String markdown) {
-        //comment
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
